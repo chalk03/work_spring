c52815314e7a72be83ccf2d68384fd06afa6ee77
KOITT <KOITT@KOITT-PC07>
2018-02-20 15:27:24 +0900

    Spring 실습예제 5번 - MyBatis 적용후

---
+++ SpringEx05/sql/bookmgr.sql
@@ -0,0 +1,21 @@
+DROP table book;
+
+CREATE table book (
+ 	isbn INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ 	title VARCHAR(30) NOT NULL,
+ 	author VARCHAR(10) NOT NULL,
+ 	publisher VARCHAR(30) NOT NULL,
+ 	price	INT NOT NULL,
+ 	description VARCHAR(255)
+ );
+ 
+INSERT INTO book (title, author, publisher, price, description)
+	VALUES ('제목-1', '저자-1', '출판사-1', 10000, '책설명-1');
+
+INSERT INTO book (title, author, publisher, price, description)
+	VALUES ('제목-2', '저자-2', '출판사-2', 20000, '책설명-2');
+	
+INSERT INTO book (title, author, publisher, price, description)
+	VALUES ('제목-3', '저자-3', '출판사-3', 10000, '책설명-3');
+	
+SELECT * FROM book;
---
+++ SpringEx05/src/com/koitt/book/dao/BookDao.java
@@ -0,0 +1,28 @@
+package com.koitt.book.dao;
+
+import java.util.List;
+
+import com.koitt.book.model.Book;
+import com.koitt.book.model.BookException;
+
+
+public interface BookDao {
+
+	// 도서 등록
+	public void insert(Book isbn) throws BookException;
+
+	// 도서번호를 이용하여 책 불러오기
+	public Book select(String isbn) throws BookException;
+
+	// 전체 도서 불러오기
+	public List<Book> selectAll() throws BookException;
+
+	// 도서 개수 가져오기
+	public int bookCount() throws BookException;
+
+	// 도서 수정하기
+	public void update(Book book) throws BookException;
+
+	// 도서 삭제하기
+	public void delete(String isbn) throws BookException;
+}
