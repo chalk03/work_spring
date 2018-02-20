68cb779c20f521ac5717d8b1a5be7a87bb1ea52c
KOITT <KOITT@KOITT-PC07>
2018-02-20 14:54:50 +0900

    Spring 16번 예제 - 파일 수정

--- Spring15BoardMVCMyBatisFileUpload/src/com/koitt/board/service/FileService.java
+++ Spring15BoardMVCMyBatisFileUpload/src/com/koitt/board/service/FileService.java
@@ -21,5 +21,5 @@ public interface FileService {
 	public void remove(HttpServletRequest request, String filename) throws FileException;
 	
 	// 파일 저장 경로 가져오기
-	public String getImgPath(HttpServletRequest request, String filename) throws FileException;
+	public String getImgPath(HttpServletRequest request, String filename);
 }
